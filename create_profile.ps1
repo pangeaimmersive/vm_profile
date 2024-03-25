9e17e31bc2131fd10cefc8436e7796c6dee30103
pangeaimmersive <161491662+pangeaimmersive@users.noreply.github.com>
2024-03-25 19:38:22 -0400

    Update create_profile.ps1

--- create_profile.ps1
+++ create_profile.ps1
@@ -102,4 +102,84 @@ New-Item -Path "$PSHOME\profile.ps1" -ItemType File -Value $Content
 
 
 #Set Microsoft Edge Bookmarks
+$Bookmarks =
+"{
+   "checksum": "8f869805b41521ce1d85096ea48498ec",
+   "roots": {
+      "bookmark_bar": {
+         "children": [ {
+            "date_added": "13355882491996907",
+            "date_last_used": "0",
+            "guid": "b57df2a8-aa82-45a4-93db-d3b9680197ce",
+            "id": "7",
+            "meta_info": {
+               "power_bookmark_meta": ""
+            },
+            "name": "Microsoft Ads",
+            "show_icon": false,
+            "source": "unknown",
+            "type": "url",
+            "url": "https://ads.microsoft.com/"
+         }, {
+            "date_added": "13355883173243545",
+            "date_last_used": "0",
+            "guid": "b1e590bb-73a0-4c00-9b31-c17fbc11a8b0",
+            "id": "8",
+            "meta_info": {
+               "power_bookmark_meta": ""
+            },
+            "name": "Gmail (marketing.pangeaimmersive@gmail.com)",
+            "show_icon": false,
+            "source": "unknown",
+            "type": "url",
+            "url": "https://mail.google.com/mail/u/4/#inbox/QgrcJHrtwMVktkXKsXQKZzBfJNxFQdxsMmb"
+         }, {
+            "date_added": "13355883372076036",
+            "date_last_used": "0",
+            "guid": "db61dac0-d29e-4a4f-afaa-93f47aecc7eb",
+            "id": "11",
+            "meta_info": {
+               "power_bookmark_meta": ""
+            },
+            "name": "Notes",
+            "show_icon": false,
+            "source": "unknown",
+            "type": "url",
+            "url": "https://github.com/pangeaimmersive/affiliate_marketing_operations/tree/main/production/notes"
+         } ],
+         "date_added": "13355882457184478",
+         "date_last_used": "0",
+         "date_modified": "13355883372076036",
+         "guid": "0bc5d13f-2cba-5d74-951f-3f233fe6c908",
+         "id": "1",
+         "name": "Favorites bar",
+         "source": "unknown",
+         "type": "folder"
+      },
+      "other": {
+         "children": [  ],
+         "date_added": "13355882457184480",
+         "date_last_used": "0",
+         "date_modified": "0",
+         "guid": "82b081ec-3dd3-529c-8475-ab6c344590dd",
+         "id": "2",
+         "name": "Other favorites",
+         "source": "unknown",
+         "type": "folder"
+      },
+      "synced": {
+         "children": [  ],
+         "date_added": "13355882457184482",
+         "date_last_used": "0",
+         "date_modified": "0",
+         "guid": "4cf2e351-0e85-532b-bb37-df045d8f8d0f",
+         "id": "3",
+         "name": "Mobile favorites",
+         "source": "unknown",
+         "type": "folder"
+      }
+   },
+   "version": 1
+}"
+
 New-Item -Path "$HOME\AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks" -ItemType File -Value $Bookmarks
