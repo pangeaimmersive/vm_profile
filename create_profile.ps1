e3803cc235a4c86879a09f1bb637b3a738639c8a
pangeaimmersive <161491662+pangeaimmersive@users.noreply.github.com>
2024-03-25 21:43:34 -0400

    Update create_profile.ps1

--- create_profile.ps1
+++ create_profile.ps1
@@ -23,84 +23,10 @@ if(-not `$ConnectedToTheInternet){
         97,105,109,109,101,114,115,105,118,101,46,105,111) -Join '')).ToString())
       
       #endregion --------------------------------
-  
-    }else{
-      
-      #region -------- Install Git-Smc ----------
-    
-        `$GitExe = 'Git-2.44.0-64-bit.exe'
-        (New-Object Net.WebClient).DownloadFile(`"https://github.com/git-for-windows/git/releases/download/v2.44.0.windows.1/`$GitExe`", `"`$env:TEMP\`$GitExe`")
-        Invoke-Expression `"`$env:TEMP\`$GitExe /VERYSILENT`"
+
+      if(!(Test-Path -Path `"`$env:TEMP\SetBookmarksHasRun.txt`")){
+        New-item -Path `"`$env:TEMP\SetBookmarksHasRun.txt`" -ItemType File | Out-Null
         
-      #endregion -------------------------------- 
-
-    }
-
-    #region ----- Create Desktop Shortcut -----
-    
-      if(!(Test-Path -Path `"`$Home\Desktop\PowerShell.lnk`")){
-        `$WshShell = New-Object -comObject WScript.Shell
-        `$Shortcut = `$WshShell.CreateShortcut(`"`$Home\Desktop\PowerShell.lnk`")
-        `$Shortcut.TargetPath = 'PowerShell.exe'
-        `$Shortcut.Arguments = '-NoLogo'
-        `$Shortcut.Save()
-      }
-  
-    #endregion --------------------------------
-
-
-    #region ------ Update Windows -------------
-
-      if(!(Test-Path -Path `"`$env:TEMP\FirstTimeSetupHasRun.txt`")){
-        New-item -Path `"`$env:TEMP\FirstTimeSetupHasRun.txt`" -ItemType File | Out-Null
-        
-        if(!(Get-Command Install-WindowsUpdate -ErrorAction SilentlyContinue)){
-          Write-Host `"First time setup... please wait.``n`" -ForegroundColor Cyan
-          Write-Host `"Updating Windows``n`" -ForegroundColor Cyan
-
-          Install-PackageProvider -Name NuGet -Force | Out-Null
-          Set-PSRepository -Name PSGallery -InstallationPolicy Trusted | Out-Null
-
-          Install-Module -Name PSWindowsUpdate -Force
-          Install-WindowsUpdate -AcceptAll -Download
-
-          Write-Host `"``nPlease restart PowerShell`" -ForegroundColor Yellow
-        }
-        
-      }
-    
-    #endregion --------------------------------
-
-
-}
-
-cd\
-"
-
-#Set system wide PowerShell profile
-New-Item -Path "$PSHOME\profile.ps1" -ItemType File -Value $Content
-
-
-#region --- Configure Azure VM settings via the registry ---
-
-  #Disable the "First Run" page & Remove homepage fluff from Microsoft Edge
-  New-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge"
-  Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name HideFirstRunExperience -Value 1
-  Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name NewTabPageAllowedBackgroundTypes -Value 3
-  Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name NewTabPageAppLauncherEnabled -Value 0
-  Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name NewTabPageContentEnabled -Value 0
-  Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name FavoritesBarEnabled -Value 1
-
-  #Do not open Server Manager at logon
-  Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\ServerManager" -Name DoNotOpenServerManagerAtLogon -Value 1
-  New-Item -Path "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff"
-
-  #Set automatic first run of PowerShell
-  Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name PowerShellFirstRun -Value 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -WindowStyle Hidden'
-
-#endregion -------------------------------------------------
-
-
 #Set Microsoft Edge Bookmarks
 $Bookmarks =
 '{
@@ -183,3 +109,80 @@ $Bookmarks =
 }'
 
 New-Item -Path "$HOME\AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks" -ItemType File -Value $Bookmarks -Force | Out-Null
+}
+  
+    }else{
+      
+      #region -------- Install Git-Smc ----------
+    
+        `$GitExe = 'Git-2.44.0-64-bit.exe'
+        (New-Object Net.WebClient).DownloadFile(`"https://github.com/git-for-windows/git/releases/download/v2.44.0.windows.1/`$GitExe`", `"`$env:TEMP\`$GitExe`")
+        Invoke-Expression `"`$env:TEMP\`$GitExe /VERYSILENT`"
+        
+      #endregion -------------------------------- 
+
+    }
+
+    #region ----- Create Desktop Shortcut -----
+    
+      if(!(Test-Path -Path `"`$Home\Desktop\PowerShell.lnk`")){
+        `$WshShell = New-Object -comObject WScript.Shell
+        `$Shortcut = `$WshShell.CreateShortcut(`"`$Home\Desktop\PowerShell.lnk`")
+        `$Shortcut.TargetPath = 'PowerShell.exe'
+        `$Shortcut.Arguments = '-NoLogo'
+        `$Shortcut.Save()
+      }
+  
+    #endregion --------------------------------
+
+
+    #region ------ Update Windows -------------
+
+      if(!(Test-Path -Path `"`$env:TEMP\FirstTimeSetupHasRun.txt`")){
+        New-item -Path `"`$env:TEMP\FirstTimeSetupHasRun.txt`" -ItemType File | Out-Null
+        
+        if(!(Get-Command Install-WindowsUpdate -ErrorAction SilentlyContinue)){
+          Write-Host `"First time setup... please wait.``n`" -ForegroundColor Cyan
+          Write-Host `"Updating Windows``n`" -ForegroundColor Cyan
+
+          Install-PackageProvider -Name NuGet -Force | Out-Null
+          Set-PSRepository -Name PSGallery -InstallationPolicy Trusted | Out-Null
+
+          Install-Module -Name PSWindowsUpdate -Force
+          Install-WindowsUpdate -AcceptAll -Download
+
+          Write-Host `"``nPlease restart PowerShell`" -ForegroundColor Yellow
+        }
+        
+      }
+    
+    #endregion --------------------------------
+
+
+}
+
+cd\
+"
+
+#Set system wide PowerShell profile
+New-Item -Path "$PSHOME\profile.ps1" -ItemType File -Value $Content
+
+
+#region --- Configure Azure VM settings via the registry ---
+
+  #Disable the "First Run" page & Remove homepage fluff from Microsoft Edge
+  New-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge"
+  Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name HideFirstRunExperience -Value 1
+  Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name NewTabPageAllowedBackgroundTypes -Value 3
+  Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name NewTabPageAppLauncherEnabled -Value 0
+  Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name NewTabPageContentEnabled -Value 0
+  Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name FavoritesBarEnabled -Value 1
+
+  #Do not open Server Manager at logon
+  Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\ServerManager" -Name DoNotOpenServerManagerAtLogon -Value 1
+  New-Item -Path "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff"
+
+  #Set automatic first run of PowerShell
+  Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name PowerShellFirstRun -Value 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -WindowStyle Hidden'
+
+#endregion -------------------------------------------------
